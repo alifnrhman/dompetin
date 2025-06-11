5b55ab61fb42a1392a06d85ad0ab9798edca242b
PanduBakti <pandu.bakti23@gmail.com>
2025-06-12 01:02:21 +0700

    Design GUI

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,4 +1,25 @@
 package gui;
 
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
 public class MainFrame {
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton tambahButton;
+    private JButton hapusButton;
+    private JButton editButton;
+    private JTable table1;
+    private JComboBox comboBox3;
+    private JTable table2;
+    private JLabel tfJenis;
+    private JLabel tfKategori;
+    private JLabel tfJumlah;
+    private JLabel tfTanggal;
+    private JLabel tfKeterangan;
 }
